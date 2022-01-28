50fd47e8618540504bd133b56167d46b340c4dee
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2022-01-28 09:59:14 +0530

    Check if comments exist before adding comments in script

--- .github/public/scripts/add-comments.sh
+++ .github/public/scripts/add-comments.sh
@@ -1,16 +1,20 @@
 #! /bin/bash
 
-# Find the line number of Recent Activity
-ln=$(grep -wn "#### ⚡ Recent Activity" README.md | cut -d: -f1)
+# Check if the starting comment exists and continue only if not
+if ! grep -Fxq '<!--START_SECTION:activity-->' README.md
+then
+    # Find the line number of Recent Activity
+    ln=$(grep -wn "#### ⚡ Recent Activity" README.md | cut -d: -f1)
 
-# Increment it by 3
-ln=$((ln + 3))
+    # Increment it by 3
+    ln=$((ln + 3))
 
-# Add the starting comment
-sed -i "${ln} i\<\!--START_SECTION:activity-->" README.md
+    # Add the starting comment
+    sed -i "${ln} i\<\!--START_SECTION:activity-->" README.md
 
-# Increment line number again by 12
-ln=$((ln + 12))
+    # Increment line number again by 12
+    ln=$((ln + 12))
 
-# Add the ending comment
-sed -i "${ln} i\<\!--END_SECTION:activity-->" README.md
+    # Add the ending comment
+    sed -i "${ln} i\<\!--END_SECTION:activity-->" README.md
+fi
